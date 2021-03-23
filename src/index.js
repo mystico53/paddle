7262cea7967958b3105b051de13fe84e04dfd8f9
konka <konkaiser@gmail.com>
2021-03-23 21:19:15 +0100

    jo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Paddle from "/src/paddle.js";
+import InputHandler from "/src/input.js";
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
@@ -9,10 +10,9 @@ const GAME_HEIGHT = 400;
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 
-paddle.draw(ctx);
+new InputHandler(paddle);
 
 let lastTime = 0;
-
 
 function gameLoop (timestamp){
 let deltaTime = timestamp - lastTime;
